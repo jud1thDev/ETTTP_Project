e9647cfbdff6d15769542fa9dedd6b1f51557eae
judith <8w8u8@naver.com>
2025-05-27 02:04:45 +0900

    feat: fill ETTP_Client, Server code

--- ETTTP_Client.py
+++ ETTTP_Client.py
@@ -5,7 +5,7 @@ import tkinter as tk
 from socket import *
 import _thread
 
-from ETTTP_TicTacToe_skeleton import TTT, check_msg
+from ETTTP_TicTacToe import TTT, check_msg
     
 
 
@@ -20,18 +20,39 @@ if __name__ == '__main__':
     
     with socket(AF_INET, SOCK_STREAM) as client_socket:
         client_socket.connect(SERVER_ADDR)  
+        print("[CLIENT] Connected to server.")
         
-        ###################################################################
-        # Receive who will start first from the server
-    
-    
-        ######################### Fill Out ################################
-        # Send ACK 
+        # 서버로부터 시작 메세지 수신
+        start_msg = client_socket.recv(SIZE).decode()
+        print("[CLIENT] Received message:\n" + start_msg)
+
+        # 메시지 형식 검사
+        if not check_msg(start_msg, SERVER_IP):
+            client_socket.close()
+            exit()
+
+        # 내가 시작인지 아닌지 판단
+        try:
+            lines = start_msg.split("\r\n")
+            server_first_move = lines[2].split(":")[1].strip()
+
+            if server_first_move == "YOU":
+                start = 1  # client starts
+                client_ack_move = "ME"
+            else:
+                start = 0  # server starts
+                client_ack_move = "YOU"
+        except Exception as e:
+            print(f"[CLIENT] Failed to parse message: {e}")
+            client_socket.close()
+            exit()
+
+       # ACK 메시지 서버로 송신
+        ack_msg = f"ACK ETTTP/1.0\r\nHost:{MY_IP}\r\nFirst-Move:{client_ack_move}\r\n\r\n"
+        print("[CLIENT] Sending ACK:\n" + ack_msg)
+        client_socket.send(ack_msg.encode())
         
-        
-        ###################################################################
-        
-        # Start game
+        # 게임 시작!
         root = TTT(target_socket=client_socket, src_addr=MY_IP,dst_addr=SERVER_IP)
         root.play(start_user=start)
         root.mainloop()
--- ETTTP_Server.py
+++ ETTTP_Server.py
@@ -5,7 +5,7 @@ import tkinter as tk
 from socket import *
 import _thread
 
-from ETTTP_TicTacToe_skeleton import TTT, check_msg
+from ETTTP_TicTacToe import TTT, check_msg
 
     
 if __name__ == '__main__':
@@ -13,31 +13,57 @@ if __name__ == '__main__':
     global send_header, recv_header
     SERVER_PORT = 12000
     SIZE = 1024
+    MY_IP = '127.0.0.1'
+
     server_socket = socket(AF_INET,SOCK_STREAM)
     server_socket.bind(('',SERVER_PORT))
     server_socket.listen()
-    MY_IP = '127.0.0.1'
+    print('[SERVER] Waiting for incoming connection...')
     
     while True:
         client_socket, client_addr = server_socket.accept()
-        
-        start = random.randrange(0,2)   # select random to start
-        
-        ###################################################################
-        # Send start move information to peer
+        print(f"[SERVER] Connected to {client_addr}")
     
-    
-        ######################### Fill Out ################################
-        # Receive ack - if ack is correct, start game
+        # 누가 먼저 시작할지 랜덤으로 결정
+        start = random.randrange(0,2) 
+        if start == 0:
+            first_player = "ME" 
+        else:
+            first_player = "YOU"
+
+        # 시작 메시지 만들어서 클라이언트한테 송신
+        start_msg = f"SEND ETTTP/1.0\r\nHost:{client_addr[0]}\r\nFirst-Move:{first_player}\r\n\r\n"
+        client_socket.send(start_msg.encode())
         
+        # 클라이언트로부터 ACK 수신
+        ack_msg = client_socket.recv(SIZE).decode()
+        print("[SERVER] Received ACK:\n" + ack_msg)
+
+        if not check_msg(ack_msg, MY_IP):
+            print("[SERVER] Invalid ACK message. Closing socket.")
+            client_socket.close()
+            break
+
+        # 클라이언트가 보낸 First-Move 값이 서버가 보낸 거랑 맞는지 확인
+        try:
+            ack_lines = ack_msg.split('\r\n')
+            ack_first_move = ack_lines[2].split(':')[1].strip()
+
+            if (start == 0 and ack_first_move != "ME") or (start == 1 and ack_first_move != "YOU"):
+                print("[SERVER] ACK First-Move mismatch. Closing connection.")
+                client_socket.close()
+                continue
+        except Exception as e:
+            print(f"[SERVER] ACK Parsing Error: {e}")
+            client_socket.close()
+            continue
         
-        ###################################################################
-        
+        # 게임 시작!
         root = TTT(client=False,target_socket=client_socket, src_addr=MY_IP,dst_addr=client_addr[0])
         root.play(start_user=start)
         root.mainloop()
         
         client_socket.close()
-        
         break
+
     server_socket.close()
