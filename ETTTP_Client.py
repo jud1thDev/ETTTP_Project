76c46a03664e6e91c19d9318950220c99cbd1891
judith <8w8u8@naver.com>
2025-05-30 16:41:35 +0900

    fix: 프린트 로그 형식 통일

--- ETTTP_Client.py
+++ ETTTP_Client.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         
         # 서버로부터 시작 메세지 수신
         start_msg = client_socket.recv(SIZE).decode()
-        print("[CLIENT] Message received:\n" + start_msg)
+        print("[RECV] Message received:\n" + start_msg)
 
         # 메시지 형식 검사
         if not check_msg(start_msg, SERVER_IP):
@@ -49,7 +49,7 @@ if __name__ == '__main__':
 
        # ACK 메시지 서버로 송신
         ack_msg = f"ACK ETTTP/1.0\r\nHost:{MY_IP}\r\nFirst-Move:{client_ack_move}\r\n\r\n"
-        print("[CLIENT] Sending ACK:\n" + ack_msg)
+        print("[SEND] Sending ACK:\n" + ack_msg)
         client_socket.send(ack_msg.encode())
         
         # 게임 시작!
